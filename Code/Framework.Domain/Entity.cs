ed448cb962780ffe2759884bc2a08c9f96a5d5ea
Mehdi Payervand <mehdipayervand@gmail.com>
2018-08-27 08:42:37 +0430

    Formatting code

--- Code/Framework.Domain/Entity.cs
+++ Code/Framework.Domain/Entity.cs
@@ -8,7 +8,7 @@ namespace Framework.Domain
 {
     public abstract class Entity<TKey>
     {
-        public TKey Id { get;protected set; }
+        public TKey Id { get; protected set; }
 
         protected bool Equals(Entity<TKey> other)
         {
@@ -20,7 +20,7 @@ namespace Framework.Domain
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
-            return Equals((Entity<TKey>) obj);
+            return Equals((Entity<TKey>)obj);
         }
 
         public override int GetHashCode()
