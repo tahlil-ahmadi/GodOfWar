d0158000bb3d42f7326aa6d97235e037ac8f7719
Abbas <khalilikhoo2@gmail.com>
2018-09-29 23:36:51 +0330

    Refactor Selective constraint and add option builder

---
+++ Code/ProductManagement.Domain.Tests.Unit/Model/Products/ProductConstraints/OptionBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProductManagement.Domain.Model.Product.ProductConstraints;
+
+namespace ProductManagement.Domain.Tests.Unit.Model.Products.ProductConstraints
+{
+    internal class OptionBuilder
+    {
+        private string _title;
+        private long _value;
+
+        public Option Build()
+        {
+            return new Option(_title, _value);
+        }
+
+        public List<Option> BuildListOptionsFrom(params Option[] options)
+        {
+            var listOptions = options.ToList();
+            return listOptions;
+        }
+
+        public OptionBuilder WithTitle(string title)
+        {
+            this._title = title;
+            return this;
+        }
+
+        public OptionBuilder WithValue(long value)
+        {
+            this._value = value;
+            return this;
+        }
+    }
+}
--- Code/ProductManagement.Domain.Tests.Unit/Model/Products/ProductConstraints/SelectiveConstraintTests.cs
+++ Code/ProductManagement.Domain.Tests.Unit/Model/Products/ProductConstraints/SelectiveConstraintTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using FluentAssertions;
-using ProductManagement.Domain.Model.Constraints;
 using ProductManagement.Domain.Model.Product.ProductConstraints;
 using Xunit;
 
@@ -9,15 +7,25 @@ namespace ProductManagement.Domain.Tests.Unit.Model.Products.ProductConstraints
 {
     public class SelectiveConstraintTests
     {
-        //TODO: refactor this tests
+        private readonly ConstraintBuilder _constraintBuilder;
+        private readonly OptionBuilder _optionBuilder;
+
+        public SelectiveConstraintTests()
+        {
+            _constraintBuilder = new ConstraintBuilder();
+            _optionBuilder =  new OptionBuilder();
+        }
 
         [Fact]
         public void Constructor_should_create_selective_constraint()
         {
-            var fuelType = new Constraint {Id = 1, Title = "FuelType"};
-            var pet = new Option("Petrolium",1);
-            var gas = new Option("Gasoline",2);
-            var options = new List<Option>() {pet,gas};
+            var constraintId = 25;
+            var constraintTilte = "FuelType";
+
+            var fuelType = _constraintBuilder.WithId(constraintId).WithTitle(constraintTilte).Build();
+            var pet = _optionBuilder.WithTitle("Petrolium").WithValue(1).Build();
+            var gas = _optionBuilder.WithTitle("Gasoline").WithValue(2).Build();
+            var options = _optionBuilder.BuildListOptionsFrom(pet, gas);
 
             var constraint = new SelectiveConstraint(fuelType, options);
 
@@ -28,10 +36,10 @@ namespace ProductManagement.Domain.Tests.Unit.Model.Products.ProductConstraints
         [Fact]
         public void Constructor_should_throw_if_options_have_duplicate_values()
         {
-            var fuelType = new Constraint { Id = 1, Title = "FuelType" };
-            var pet = new Option("Petrolium", 1);
-            var gas = new Option("Gasoline", 1);
-            var options = new List<Option>() { pet, gas };
+            var fuelType = _constraintBuilder.Build();
+            var pet = _optionBuilder.WithTitle("Petrolium").WithValue(1).Build();
+            var gas = _optionBuilder.WithTitle("Gasoline").WithValue(1).Build();
+            var options = _optionBuilder.BuildListOptionsFrom(pet, gas);
 
             Action constructor = ()=> new SelectiveConstraint(fuelType, options);
 
